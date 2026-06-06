53a616353c013625d3f90deac2dd0069015b7860
Aaradhya-Dixit <dixitaaradhya03@gmail.com>
2026-06-06 17:55:22 +0530

    Added day9 program

---
+++ day9/problem35.java
@@ -0,0 +1,13 @@
+package day9;
+
+public class problem35 {
+    
+       public static void main(String[] args) {
+        for (char i = 'A'; i <= 'E'; i++) {
+            for (char j = 'A'; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
