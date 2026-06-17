d416c96a9045be405c6a769e77a4c326a249f283
Aaradhya-Dixit <dixitaaradhya03@gmail.com>
2026-06-17 19:16:11 +0530

    Added day17 program

---
+++ day17/problem68.java
@@ -0,0 +1,34 @@
+package day17;
+
+import java.util.Scanner;
+
+public class problem68 {
+    public static void main(String[] args) {
+         System.out.println("Enter the size of first array=");
+        Scanner sc = new Scanner(System.in);
+        int n1= sc.nextInt();
+        int arr1[]= new int[n1];
+         System.out.println("Enter the elements of array1=");
+         for(int i = 0;i<n1;i++){
+            arr1[i]=sc.nextInt();
+        }
+        System.out.println("Enter the size of second array=");
+        int n2= sc.nextInt();
+        int arr2[]= new int[n2];
+         System.out.println("Enter the elements of array2=");
+        for(int i = 0;i<n2;i++){
+            arr2[i]=sc.nextInt();
+        }
+    
+
+for (int i = 0; i < arr1.length; i++) {
+    for (int j = 0; j < arr2.length; j++) {
+        if (arr1[i] == arr2[j]) {
+            System.out.print(arr1[i] + " ");
+        }
+    }
+}
+sc.close();
+    }
+    }
+
